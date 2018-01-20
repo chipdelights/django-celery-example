fe996cdb9499634f70a7655047ea87d8b67fd141
bseenu <bseenu@yahoo-inc.com>
2018-01-19 23:21:08 -0800

    Adding nginx to route the requests through uwsgi

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 manage.py migrate --run-syncdb
-python3 manage.py runserver 0.0.0.0:8000 >> django.log 2>&1
+/usr/local/bin/uwsgi --ini /etc/uwsgi/uwsgi.ini
+/usr/sbin/nginx -g "daemon off;"
